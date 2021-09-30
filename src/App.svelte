ea2161ba84fdc9e545499a0470087dc02237d209
RJay Sharma <sharma.rajan3097@gmail.com>
2021-09-30 16:05:33 +0530

    inline event handler

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,11 @@
 		{name: 'Sudhanshu', beltColor: 'orange', age: 26, id: 2},
 		{name: 'Prabh', beltColor: 'brown', age: 25, id: 3}
 	];
+
+	const handleCLick = (id) => {
+		people = people.filter((person) => person.id != id);
+	}
+
 </script>
 
 <main>
@@ -11,6 +16,7 @@
 		<div>
 			<h4>{person.name}</h4>
 			<p>{person.age} years old, {person.beltColor} belt.</p>
+			<button on:click={() => handleCLick(person.id) }>Delete</button>
 		</div>
 		{:else}
 			<p>There are no people to show..</p>
