edc4ddd5588d7c246ae899f3c48493ef5d7b85a4
RJay Sharma <sharma.rajan3097@gmail.com>
2021-10-12 14:50:26 +0530

    implemented poll project

--- Svelte-Polls-Project/src/App.svelte
+++ Svelte-Polls-Project/src/App.svelte
@@ -2,6 +2,7 @@
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
 	import CreatePollForm from './components/CreatePollForm.svelte';
+	import PollList from './components/PollList.svelte';
 	import Tabs from './shared/Tabs.svelte';
 
 	// tabs
@@ -12,15 +13,49 @@
 		activeItem = e.detail;
 	}
 
+	// polls
+	let polls = [
+		{
+			id: 1,
+			question: 'Python or JavaScript?',
+			answerA: 'Python',
+			answerB: 'JavaScript',
+			votesA: 9,
+			votesB: 15,
+		},
+	];
+
+	const handleAdd = (e) => {
+		const poll = e.detail;
+		polls = [poll, ...polls];
+		console.log(polls);
+		activeItem = 'Current Polls';
+	}
+
+	const handleVote = (e) => {
+		const { id, option } = e.detail;
+
+		let copiedPolls = [...polls];
+		let upVotedPoll = copiedPolls.find(poll => poll.id == id);
+
+		if(option === 'a') {
+			upVotedPoll.votesA++;
+		} else {
+			upVotedPoll.votesB++;
+		}
+
+		polls = copiedPolls;
+	}
+
 </script>
 
 <Header/>
 <main>
 	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
 	{#if activeItem === 'Current Polls'}
-		<p>Poll List Component goes here</p>
+		<PollList {polls} on:vote={handleVote}/>
 	{:else if activeItem === 'Add New Poll'}
-		<CreatePollForm />
+		<CreatePollForm on:add={handleAdd}/>
 	{/if}
 </main>
 <Footer />
--- Svelte-Polls-Project/src/components/CreatePollForm.svelte
+++ Svelte-Polls-Project/src/components/CreatePollForm.svelte
@@ -1,9 +1,44 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
   import Button from '../shared/Button.svelte';
+
+  let dispatch = createEventDispatcher();
+
   let fields = { question: '', answerA: '', answerB: '' };
+  let errors = { question: '', answerA: '', answerB: '' };
+  let valid = false;
 
   const submitHandler = () => {
-    console.log(fields)
+    valid = true;
+
+    // validate question
+    if(fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question must be at least 5 characters long.";
+    } else {
+      errors.question = "";
+    }
+
+    if(fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = "Answer A cannot be empty.";
+    } else {
+      errors.answerA = "";
+    }
+
+    if(fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = "Answer B cannot be empty.";
+    } else {
+      errors.answerB = "";
+    }
+
+    // add new poll
+    if(valid) {
+      let poll = {...fields, votesA: 0, votesB: 0, id: Math.random()};
+      dispatch('add', poll);
+    }
+
   }
 </script>
 
@@ -11,14 +46,17 @@
   <div class="form-field">
     <label for="question">Poll Question:</label>
     <input type="text" id="question" bind:value={fields.question}>
+    <div class="error">{errors.question}</div>
   </div>
   <div class="form-field">
     <label for="answer-a">Answer A:</label>
     <input type="text" id="answer-a" bind:value={fields.answerA}>
+    <div class="error">{errors.answerA}</div>
   </div>
   <div class="form-field">
     <label for="answer-b">Answer B:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB}>
+    <div class="error">{errors.answerB}</div>
   </div>
   <Button type='secondary' flat={true} >Add Poll</Button>
 </form>
@@ -40,5 +78,10 @@
   label {
     margin: 10px auto;
     text-align: left;
+  }
+  .error {
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
   }
 </style>
